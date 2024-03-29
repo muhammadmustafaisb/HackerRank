2f7f23094fef269e5c397b580f9106aec46a1f45
muhammadmustafa <muhammadmustafaisb@gmail.com>
2024-03-29 16:14:07 +0500

    Day-2 Question 1

---
+++ HackerRankTeam.Tests/Lonely Integer/Lonely-IntegerTests.cs
@@ -0,0 +1,44 @@
+﻿namespace HackerRankTeam.UnitTest.Lonely_Integer
+{
+    [TestFixture]
+    public class Lonely_IntegerTests
+    {
+        [Test]
+        [TestCase(new int[] { 1, 1, 2, 2, 3 }, 3)]
+        [TestCase(new int[] { 4, 4, 5, 7, 5, 6, 6}, 7)]
+        [TestCase(new int[] { 9, 8, 7, 8, 9 }, 7)]
+        public void TestFindUniqueElement(int[] arr,int Output)
+        {
+ 
+
+            var result = Lonelyinteger(arr);
+
+            Assert.That(result, Is.EqualTo(Output));
+        }
+
+        public static int Lonelyinteger(int[] arr)
+        {
+            int uniqueElement = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+
+                }
+                if (count == 1)
+                {
+                    uniqueElement = arr[i];
+                    break;
+                }
+            }
+            return uniqueElement;
+        }
+    }
+}
---
+++ HackerRankTeam/Lonely Integer/LonelyInteger.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+
+namespace HackerRankTeam.Lonely_Integer
+{
+    public class LonelyInteger
+    {
+        public static int FindUniqueElement(List<int> arr)
+        {
+            int uniqueElement = 0;
+
+            for (int i = 0; i < arr.Count; i++) 
+            {
+                int count = 0;
+
+                for (int j = 0; j < arr.Count; j++) 
+                {
+                    if (arr[i] == arr[j]) 
+                    { 
+                        count++;
+                    }
+                }
+
+                if (count == 1)
+                {
+                    uniqueElement = arr[i];
+                    break;
+                }
+            }
+
+            return uniqueElement;
+        }
+    }
+}
